a63b418940d7a69467772d525da7164d14f6892f
Jeanne M Berthy <jberthy39@ccs.local>
2020-03-12 12:50:48 -0400

    I am trying to mak emy code more efficient

--- Arduino.coding.for.robot/buzzer.and.sensor/sensor.ino
+++ Arduino.coding.for.robot/buzzer.and.sensor/sensor.ino
@@ -56,12 +56,12 @@
 Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
 // ultrasonic sensor setup:
 #include <NewPing.h>
-#include <NewTone.h>
 #define TRIGGER_PIN 6 // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN 2 // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 // buzzer setup:
+#include <NewTone.h>
 int buzzer = 9;
 int sadCounter = 0;
 int happyCounter = 0;
@@ -81,13 +81,6 @@ void setup()
 	pinMode(RbluePin, OUTPUT);
 	pinMode(LbluePin, OUTPUT);
 	matrix.begin(0x70);
-	// false = sad
-	// true = happy
-	// 
-	// 
-	// 
-	// 
-	// 
 }
 
 static const uint8_t PROGMEM
@@ -203,11 +196,6 @@ ecstaticness[] =
 	B00000000
 };
 
-// 
-// 
-// 
-// 
-// 
 void loop()
 {
 	unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
@@ -215,26 +203,20 @@ void loop()
 	Serial.print("Ping: ");
 	Serial.print(uS); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
 	Serial.println("cm");
-	if (uS > 0 && uS < 100)
+	if (uS > 0 && uS < 50)
 	{
-
-		if (uS <= 10)
+		if (uS <= 15)
 		{
 			happysong();
-			redison();
 			happyeyes();
-			blink();
 		}
 		else
-			if (uS >= 11)
+			if (uS >= 16)
 			{
 				sadsong();
-				blueison();
 				sadeyes();
-				blink();
 			}
 	}
-	delay(100);
 }
 
 int sadsong()
